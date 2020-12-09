c7450bef10c7e8cf0771c6b26e8eccd9c04f6d55
Adesoji Susan <42998872+adesojisusan@users.noreply.github.com>
2020-12-09 23:02:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 
+# 🚨 Don't change the code below 👇
+year = int(input("Which year do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+if year % 4 == 0:
+  if year % 100 == 20:
+    print("not leap")
+    if year % 400 == 5:
+      print("leap")
+    else:
+        print("not leap")
+  else:
+   print("leap")
+else:
+  print("not leap year")
