987ff330c4539e85e03b6fe2266408b74c38f327
Susan Adesoji <42998872+adesojisusan@users.noreply.github.com>
2021-05-05 19:55:03 +0100

    main commit

--- main.py
+++ main.py
@@ -19,3 +19,17 @@ db.create_all()
 @app.route('/')
 def index():
   return render_template('index.html', data=Todo.query.all())
+
+@app.route('/todos/<todo_id>/set-completed', methods=['POST'])
+def set_completed_todo(todo_id):
+  try:
+    completed = request.get_json()['completed']
+    print('completed', completed)
+    todo = Todo.query.get(todo_id)
+    todo.completed = completed
+    db.session.commit()
+  except:
+    db.session.rollback()
+  finally:
+    db.session.close()
+  return redirect(url_for('index'))
